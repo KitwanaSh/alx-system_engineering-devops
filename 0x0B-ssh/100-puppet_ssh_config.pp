275a042b5d1c9c731e49fe2f5368f08cccc5b45f
KitwanaSh <rodriguekitweze@gmail.com>
2023-01-12 13:00:52 +0200

    Feat: add the advanced tas:

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+#configure ssh config
+
+file_line { 'No password':
+    ensure             => present,
+    path               => '/etc/ssh/ssh_config',
+    line               => '    PasswordAuthentication no',
+    match              => '^PasswordAuthentication',
+    append_on_no_match => true,
+  }
+
+file_line { 'New private key holder':
+    ensure             => present,
+    path               => '/etc/ssh/ssh_config',
+    line               => '    IdentityFile ~/.ssh/school',
+    match              => '^IdentityFile',
+    append_on_no_match => true,
+  }
